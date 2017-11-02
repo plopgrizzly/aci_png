0176449cb0bc779b95860a1d125b769657eb9f9c
Jeron Aldaron Lau <jeron.lau@plopgrizzly.com>
2017-11-02 14:13:21 -0500

    aci_png new version.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# aci_png
-Aldaron's Codec Interface - PNG: A library for decoding pngs and apngs.
+# Aldaron's Codec Interface / PNG (aci_png 0.3.0)
+Aldaron's Codec Interface / PNG is a library developed by Plop Grizzly for
+decoding pngs and apngs.
+
+[Cargo](https://crates.io/crates/aci_png) /
+[Documentation](https://docs.rs/aci_png)
+
+## Features
+**aci_png**'s current features:
+* Decode png image files
+
+**aci_png**'s planned features:
+* Encode png image files
+* Encode and decode apng files.
+
+## Support
+**aci_png**'s current support:
+* Platforms that support png crate (platforms with libc)
+
+**aci_png**'s planned support:
+* Without png crate, with optimized multithreaded rust png loading.
+* Arduino and Raspberry Pi (no os)
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,19 @@
-// Aldaron's Codec Interface - PNG
+// Aldaron's Codec Interface / PNG
 // Copyright (c) 2017 Plop Grizzly, Jeron Lau <jeron.lau@plopgrizzly.com>
 // Licensed under the MIT LICENSE
 //
 // src/lib.rs
 
+//! Aldaron's Codec Interface / PNG is a library developed by Plop Grizzly for
+//! decoding png image files.
+
+#![warn(missing_docs)]
+#![doc(html_logo_url = "http://plopgrizzly.com/icon.png",
+	html_favicon_url = "http://plopgrizzly.com/icon.png",
+	html_root_url = "http://plopgrizzly.com/aci_png/")]
+
+extern crate ami;
+extern crate afi;
 extern crate png;
 
 /// The errors that can be returned if `decode()` fails.
@@ -36,9 +46,8 @@ impl ::std::fmt::Display for DecodeErrorPNG {
 	}
 }
 
-/// Decode PNG data.  On success, returns image as vector:
-/// `[width, height, first BGRA pixel .. last BGRA pixel]`
-pub fn decode(png: &'static [u8]) -> Result<Vec<u32>, DecodeErrorPNG> {
+/// Decode PNG data.  On success, returns the png as a `Graphic`.
+pub fn decode(png: &'static [u8]) -> Result<afi::Graphic, DecodeErrorPNG> {
 	use png::ColorType::*;
 
 	let decoder = png::Decoder::new(png);
@@ -48,58 +57,49 @@ pub fn decode(png: &'static [u8]) -> Result<Vec<u32>, DecodeErrorPNG> {
 	reader.next_frame(&mut buf).unwrap();
 
 	let size = (info.width * info.height) as usize;
-	let mut out = Vec::with_capacity(size + 2);
+	let mut out : ami::Vec<u32> = ami::Vec::with_capacity(size + 2);
 
-	unsafe {
-		out.set_len(size + 2);
-	}
-
-	out[0] = info.width;
-	out[1] = info.height;
+	out.push(info.width);
+	out.push(info.height);
 
 	let (color, bit) = reader.output_color_type();
 
-	match color {
+	let graphic = match color {
 		RGB => {
-			let mut pixel: [u8; 4] = unsafe {
-				::std::mem::uninitialized()
-			};
+			let mut pixel : [u8;4] = [0xFF, 0xFF, 0xFF, 0xFF];
 
 			for i in 0..size {
-				pixel[0] = buf[i * 3 + 2];
+				pixel[0] = buf[i * 3 + 0];
 				pixel[1] = buf[i * 3 + 1];
-				pixel[2] = buf[i * 3 + 0];
-				pixel[3] = 0xFF;
+				pixel[2] = buf[i * 3 + 2];
 
-				out[2 + i] = unsafe {
-					::std::mem::transmute(pixel)
-				};
+				out.push(unsafe {::std::mem::transmute(pixel)});
 			}
+
+			afi::GraphicBuilder::new().rgba(out)
 		},
 		RGBA => {
-			let mut pixel: [u8; 4] = unsafe {
-				::std::mem::uninitialized()
-			};
+			let mut pixel : [u8;4] = [0xFF, 0xFF, 0xFF, 0xFF];
 
 			for i in 0..size {
-				pixel[0] = buf[i * 4 + 2];
+				pixel[0] = buf[i * 4 + 0];
 				pixel[1] = buf[i * 4 + 1];
-				pixel[2] = buf[i * 4 + 0];
+				pixel[2] = buf[i * 4 + 2];
 				pixel[3] = buf[i * 4 + 3];
 
-				out[2 + i] = unsafe {
-					::std::mem::transmute(pixel)
-				};
+				out.push(unsafe {::std::mem::transmute(pixel)});
 			}
+
+			afi::GraphicBuilder::new().rgba(out)
 		},
 		Grayscale => return Err(DecodeErrorPNG::GrayscaleNYI),
 		Indexed => return Err(DecodeErrorPNG::IndexedNYI),
 		GrayscaleAlpha => return Err(DecodeErrorPNG::AGrayscaleNYI),
-	}
+	};
 
 	if bit != png::BitDepth::Eight {
 		return Err(DecodeErrorPNG::BitsNYI)
 	}
 
-	Ok(out)
+	Ok(graphic)
 }
